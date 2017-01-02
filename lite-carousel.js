47466301686cc985c3c1c162d24c14083f8333e1
Dinesh T <dineshselvantdm@gmail.com>
2017-01-02 12:55:07 +0530

    Update lite-carousel.js

--- lite-carousel.js
+++ lite-carousel.js
@@ -1,17 +1,17 @@
-var CircularCarousel = {   
+ar CircularCarousel = {   
 	start_time : 0,
 	list_obj : {},
 	position : "Right",
 	delay: 500,
 	width: 20,
-	margin_Right: -3,
+	margin_left: -3,
 	computed_width: 23,
 	init : function(position,ul_selector,delay){
 		CircularCarousel.position = position;
 		CircularCarousel.delay = delay;
 		var li_array = document.querySelectorAll(ul_selector + ' li');
 		CircularCarousel.width = li_array[0].offsetWidth;
-		CircularCarousel.computed_width = CircularCarousel.width-CircularCarousel.margin_Right;
+		CircularCarousel.computed_width = CircularCarousel.width-CircularCarousel.margin_left;
 		CircularCarousel.list_obj = {li_array: li_array,selector:ul_selector};
 	},
 	animateCarousel : function(timestamp){
@@ -20,18 +20,18 @@ var CircularCarousel = {
 		var selector = CircularCarousel.list_obj.selector;
 		var progress = timestamp - CircularCarousel.start_time;
 		if(CircularCarousel.position == 'Left'){
-			var margin_Right = Math.max(CircularCarousel.computed_width-((progress*CircularCarousel.computed_width)/CircularCarousel.delay),CircularCarousel.margin_Right);
-			document.querySelector(selector + ' li:first-child').style.marginRight = (margin_Right*-1).toString()+'px';
-			if(margin_Right == CircularCarousel.margin_Right){
+			var margin_left = Math.max(CircularCarousel.computed_width-((progress*CircularCarousel.computed_width)/CircularCarousel.delay),CircularCarousel.margin_left);
+			document.querySelector(selector + ' li:first-child').style.marginLeft = (margin_left*-1).toString()+'px';
+			if(margin_left == CircularCarousel.margin_left){
 				f_child = document.querySelector(selector + ' li:last-child');
 				f_child.parentNode.removeChild(f_child);
 				CircularCarousel.start_time = 0;
 				return;
 			}
 		}else if(CircularCarousel.position == 'Right'){
-			var margin_Right = Math.min(((progress*CircularCarousel.computed_width)/CircularCarousel.delay),CircularCarousel.computed_width);
-			document.querySelector(selector + ' li:first-child').style.marginRight = (margin_Right*-1).toString()+'px';
-			if(margin_Right == CircularCarousel.computed_width){
+			var margin_left = Math.min(((progress*CircularCarousel.computed_width)/CircularCarousel.delay),CircularCarousel.computed_width);
+			document.querySelector(selector + ' li:first-child').style.marginLeft = (margin_left*-1).toString()+'px';
+			if(margin_left == CircularCarousel.computed_width){
 				f_child = document.querySelector(selector + ' li:first-child');
 				f_child.parentNode.removeChild(f_child);
 				CircularCarousel.start_time = 0;
@@ -76,7 +76,7 @@ var CircularCarousel = {
 			document.querySelector(selector).innerHTML = "";
 			document.querySelector(selector).appendChild(frag);
 			if(position == 'Left'){
-				document.querySelector(selector + ' li:first-child').style.marginRight = (CircularCarousel.computed_width*-1).toString() + "px";
+				document.querySelector(selector + ' li:first-child').style.marginLeft = (CircularCarousel.computed_width*-1).toString() + "px";
 			}
 			requestAnimationFrame(CircularCarousel.animateCarousel);
 		}	
